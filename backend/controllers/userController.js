89f37aaa73aa35433641cf607190ae061a2411d7
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-09-05 02:42:59 +0530

    made some backend changes for token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -116,10 +116,11 @@ const login = async (req, res) => {
       repositories: user.repositories,
     };
 
-    return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+    return res.cookie('token', token, { sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
       message: `Welcome back ${user.username}`,
       success: true,
       user,
+      token
     });
   } catch (error) {
     console.log(error);
