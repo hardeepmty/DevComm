edaa75fad21a64a1409f161c139966f18ca3da35
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-09-11 15:46:21 +0530

    added the methods in cors

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true 
 };
 
